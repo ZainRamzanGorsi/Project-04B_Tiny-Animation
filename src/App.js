936db59d40c4408879b9e7468475fb994c15babe
ZainRamzan <64620737+ZainRamzanGorsi@users.noreply.github.com>
2020-11-30 18:16:31 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const { ref } = useWebAnimations({
+    keyframes: [
+      { transform: "translate(0,0)" },
+      { transform: "translate(220px,0)" },
+      { transform: "translate(200px,120px)" },
+      { transform: "translate(420px,120px)" },
+      { transform: "translate(420px,270px)" },
+      { transform: "translate(630px,270px)" },
+      { transform: "translate(630px,420px)" },
+      { transform: "translate(810px,420px)" },
+      { transform: "translate(810px,580px)" },
+      { transform: "translate(1200px,580px)" },
+    ],
+
+    timing: {
+      duration: 6000, // Run for 1000ms
+      iterations: Infinity,
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container" ref={ref}></div>
+      <div className="line"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+      <div className="line4"></div>
     </div>
   );
 }
-
-export default App;
